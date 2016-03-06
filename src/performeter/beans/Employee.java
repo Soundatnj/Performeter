af809e0ecbf04a868e35e38bdbf9aaf275a539ec
hemansutanty <hemansu.tanty@gmail.com>
2016-03-06 20:01:13 +0530

    Initial commit

---
+++ src/performeter/beans/Employee.java
@@ -0,0 +1,93 @@
+package performeter.beans;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private Integer empId;
+	private String empName;
+	private String empPhNo;
+	private Date empDOB;
+	private String empAdd;
+	private String empQual;
+	private String empObj;
+	private String empPlace;
+	private String empReview;
+	private Double empObSupRt;
+	private Double empOvSelRt;
+	private Double empAvRt;
+	
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpPhNo() {
+		return empPhNo;
+	}
+	public void setEmpPhNo(String empPhNo) {
+		this.empPhNo = empPhNo;
+	}
+	public Date getEmpDOB() {
+		return empDOB;
+	}
+	public void setEmpDOB(Date empDOB) {
+		this.empDOB = empDOB;
+	}
+	public String getEmpAdd() {
+		return empAdd;
+	}
+	public void setEmpAdd(String empAdd) {
+		this.empAdd = empAdd;
+	}
+	public String getEmpQual() {
+		return empQual;
+	}
+	public void setEmpQual(String empQual) {
+		this.empQual = empQual;
+	}
+	public String getEmpObj() {
+		return empObj;
+	}
+	public void setEmpObj(String empObj) {
+		this.empObj = empObj;
+	}
+	public String getEmpPlace() {
+		return empPlace;
+	}
+	public void setEmpPlace(String empPlace) {
+		this.empPlace = empPlace;
+	}
+	public String getEmpReview() {
+		return empReview;
+	}
+	public void setEmpReview(String empReview) {
+		this.empReview = empReview;
+	}
+	public Double getEmpObSupRt() {
+		return empObSupRt;
+	}
+	public void setEmpObSupRt(Double empObSupRt) {
+		this.empObSupRt = empObSupRt;
+	}
+	public Double getEmpOvSelRt() {
+		return empOvSelRt;
+	}
+	public void setEmpOvSelRt(Double empOvSelRt) {
+		this.empOvSelRt = empOvSelRt;
+	}
+	public Double getEmpAvRt() {
+		return empAvRt;
+	}
+	public void setEmpAvRt(Double empAvRt) {
+		this.empAvRt = empAvRt;
+	}
+
+}
---
+++ src/performeter/dao/LoginDAO.java
@@ -0,0 +1,14 @@
+package performeter.dao;
+
+import performeter.beans.EmployeeLogin;
+
+public class LoginDAO {
+	
+	public boolean findEmployee(EmployeeLogin loginObj){
+		
+		
+		
+		return true;
+	}
+
+}
